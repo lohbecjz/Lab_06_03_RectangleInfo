20f9fcf2c07a1cc04cb502f0fcc61921b95f0482
lohbecjz <lohbecjz@mail.uc.edu>
2022-09-27 19:41:22 -0400

    Completed assignment.

--- src/Lab_06_03_RectangleInfo.java
+++ src/Lab_06_03_RectangleInfo.java
@@ -1,5 +1,57 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+/**
+ * @author lohbecjz@mail.uc.edu
+ */
+
+import java.util.Scanner;
+
+public class Lab_06_03_RectangleInfo
+{
+    public static void main(String[] args)
+    {
+        // Declare Variables
+        double rectangleLength = 0;
+        double rectangleWidth = 0;
+        double rectangleArea = 0;
+        double rectanglePerimeter = 0;
+        double rectangleDiagonal = 0;
+        String trash = "";
+        Scanner in = new Scanner(System.in);
+
+        // Prompt and input
+        System.out.print("Enter the length of the rectangle: ");
+        if(in.hasNextDouble())
+        {
+            rectangleLength = in.nextDouble();
+            in.nextLine(); // clear the buffer
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println(trash + " is not a valid number.");
+            System.exit(0); // terminate the program
+        }
+
+        System.out.print("Enter the width of the rectangle: ");
+        if(in.hasNextDouble())
+        {
+            rectangleWidth = in.nextDouble();
+            in.nextLine(); // clear the buffer
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println(trash + " is not a valid number.");
+            System.exit(0); // terminate the program
+        }
+
+        // Process and output
+        rectangleArea = rectangleLength * rectangleWidth;
+        rectanglePerimeter = (2 * rectangleLength) + (2 * rectangleWidth);
+        rectangleDiagonal = Math.sqrt(Math.pow(rectangleLength, 2) + Math.pow(rectangleWidth, 2));
+
+        System.out.println("The area of the rectangle is: " + rectangleArea);
+        System.out.println("The perimeter of the rectangle is: " + rectanglePerimeter);
+        System.out.printf("The length of the diagonal is : %.2f", rectangleDiagonal);
+
     }
 }
